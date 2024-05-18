89482fa24e4e800e1830630add661bcd10a12cb7
Hackcraft_ <hackcraft_@hotmail.com>
2024-05-18 14:15:07 -0400

    added cmd-c and cmd-v to neovide

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -16,4 +16,17 @@ map("n", "[t", function()
 end, { desc = "Previous todo comment" })
 
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+if vim.g.neovide then
+  map('n', '<D-s>', ':w<CR>') -- Save
+  map('v', '<D-c>', '"+y') -- Copy
+  map('n', '<D-v>', '"+P') -- Paste normal mode
+  map('v', '<D-v>', '"+P') -- Paste visual mode
+  map('c', '<D-v>', '<C-R>+') -- Paste command mode
+  map('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+end
+
+
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
